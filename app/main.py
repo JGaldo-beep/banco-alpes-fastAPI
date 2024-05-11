a988f656bbf37fb306571d1d4439d5d76e1332d1
Santiago Castro <s.castrod@uniandes.edu.co>
2024-05-10 23:58:36 -0500

    autenticacion

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from router.tarjeta import tarjeta_router
 from router.solicitud import solicitud_router
 from config.database import engine, Base
 from router.usuario import usuario_router
-from fastapi import Depends, FastAPI, HTTPException
-from authlib.integrations.starlette_client import OAuth
+from fastapi import FastAPI
+
 
 import socket
 
@@ -20,30 +20,8 @@ app.include_router(auth_router)
 app.include_router(solicitud_router)
 app.include_router(usuario_router)
 
-oauth = OAuth()
-oauth.register(
-    name='auth0',
-    client_id='uzjiRjQpkLjHZoIKvUVywVdnpiZG884A',
-    client_secret='Avk-wIMQJDqwFbkKS-hhTN-TypT8PJ0JiWBtQEzxxeYCf3JcrKbkNk4OYQxnVFCM',
-    server_metadata_url='https://dev-buva6xsj816cpyze/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid profile email'}
-)
-
 Base.metadata.create_all(bind=engine)
 
 @app.get('/')
 def root():
     return {"id": socket.gethostname()}
-
-##Auth 0
-@app.get("/profile")
-async def profile(token: str = Depends(oauth.authenticator(['auth0']))):
-    user = token.user
-    return {"user_id": user.get('sub'), "user_email": user.get('email')}
-
-@app.exception_handler(HTTPException)
-async def http_exception_handler(request, exc):
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={"message": exc.detail}
-    )
--- app/router/auth.py
+++ app/router/auth.py
@@ -1,13 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import JSONResponse
+from fastapi.security import HTTPBearer
 
-from utils.jwt_manager import create_token
-from schemas.auth import User
+from authlib.integrations.starlette_client import OAuth
 
 auth_router = APIRouter()
 
-@auth_router.post('/login', tags = ['auth'])
-def login(user: User):
-    if user.email == "admin@gmail.com" and user.password == "admin":
-        token: str = create_token(user.model_dump())
-        return JSONResponse(content=token, status_code=200)
+oauth = OAuth()
+oauth.register(
+    name='auth0',
+    client_id='uzjiRjQpkLjHZoIKvUVywVdnpiZG884A',
+    client_secret='Avk-wIMQJDqwFbkKS-hhTN-TypT8PJ0JiWBtQEzxxeYCf3JcrKbkNk4OYQxnVFCM',
+    server_metadata_url='https://dev-buva6xsj816cpyze/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid profile email'}
+)
+
+@auth_router.get("/login", tags = ['autenticacion'])
+async def login(request: Request):
+    redirect_uri = request.url_for('auth')
+    return oauth.auth0.authorize_redirect(request, redirect_uri)
+
+@auth_router.get("/auth", tags = ['autenticacion'])
+async def auth(request: Request):
+    token = await oauth.auth0.authorize_access_token(request)
+    user = await oauth.auth0.parse_id_token(request, token)
+    return {"user_id": user['sub'], "user_email": user['email']}
