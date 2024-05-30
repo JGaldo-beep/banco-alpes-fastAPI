1629518444eb4be7529691ba733b0c7aac4cf9da
LuisCastelblanco <la.castelblanco@uniandes.edu.co>
2024-05-29 22:36:27 -0500

    chore: Update tarjetas deployment image version and add Redis configuration

--- tarjetaService/main.py
+++ tarjetaService/main.py
@@ -1,20 +1,31 @@
-#from middlewares.error_handler import ErrorHandler
 from router.auth import auth_router
 from router.tarjeta import tarjeta_router
 from config.database import engine, Base
 from fastapi import FastAPI
-
 import socket
+import redis
+import os
 
 app = FastAPI()
-app.title = 'Banco de los Alpes'
+app.title = "Banco de los Alpes"
 app.version = "0.0.1"
 
-#app.add_middleware(ErrorHandler)
+# app.add_middleware(ErrorHandler)
 app.include_router(tarjeta_router)
 app.include_router(auth_router)
 
 Base.metadata.create_all(bind=engine)
+
+# Configuración de Redis
+redis_host = os.environ.get("REDIS_HOST")
+redis_port = os.environ.get("REDIS_PORT")
+
+try:
+    redis_client = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
+    redis_client.ping()  # Verificar la conexión
+except redis.ConnectionError as e:
+    print(f"Error al conectar a Redis: {e}")
+    raise  # Detener la aplicación si no se puede conectar
 
 @app.get('/')
 def root():
